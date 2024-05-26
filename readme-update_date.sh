ddb7707ffa14b860f84659645d6a6691f5580a6c
RanggaBS <ranggabuanasaputra85@gmail.com>
2024-05-26 10:17:35 +0700

    update: upload files from local

---
+++ readme-update_date.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Delete everything
+echo '' > README.md
+
+# Heading
+echo "Last Updated" >> README.md
+
+# Insert blank line
+echo '' >> README.md
+
+# Date
+echo "$(date)" >> README.md
