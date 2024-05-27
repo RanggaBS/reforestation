f9150579c870966103f73fee7304e00ce51c9c0e
Rangga Buana Saputra <95119001+RanggaBS@users.noreply.github.com>
2024-05-27 21:07:38 +0700

    fix: numeric operator

--- update_next_song_line_index.sh
+++ update_next_song_line_index.sh
@@ -6,7 +6,7 @@ CURRENT_SONG_LINE_INDEX=$(cat next_song_line_index.txt)
 # Calculate the next song line index
 # 5 > 4 > 3 > 2 > 1 > 0
 NEXT_SONG_LINE_INDEX=$((CURRENT_SONG_LINE_INDEX - 1))
-if [[ $NEXT_SONG_LINE_INDEX < 0 ]]; then
+if [[ $NEXT_SONG_LINE_INDEX -lt 0 ]]; then
     NEXT_SONG_LINE_INDEX=$((LYRICS_COUNT - 1))
 fi
 
