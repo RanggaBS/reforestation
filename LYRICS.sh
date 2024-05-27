f1ca1cc8c9de68bbab5e16e0722ea98bc86d8241
Rangga Buana Saputra <95119001+RanggaBS@users.noreply.github.com>
2024-05-27 20:43:28 +0700

    Create LYRICS.sh
    To store the song lyrics as constant variable

---
+++ LYRICS.sh
@@ -0,0 +1,11 @@
+# CONSTANTS
+
+LYRICS=(
+    "Lihat kebunku"                 # 0
+    "Penuh dengan bunga"            # 1
+    "Ada yang putih"                # 2
+    "Dan ada yang merah"            # 3
+    "Setiap hari kusiram semua"     # 4
+    "Mawar, melati, semuanya indah" # 5
+)
+LYRICS_COUNT=${#LYRICS[@]}
