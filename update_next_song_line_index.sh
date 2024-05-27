10e6efc43fd0dad76362a1e09b487b24047aae95
Rangga Buana Saputra <95119001+RanggaBS@users.noreply.github.com>
2024-05-27 20:49:28 +0700

    Create update_next_song_line_index.sh
    GitHub Actions can't write to a file, so instead just run this script to solve that problem.

---
+++ update_next_song_line_index.sh
@@ -0,0 +1,14 @@
+# Import script
+source LYRICS.sh
+
+CURRENT_SONG_LINE_INDEX=$(cat next_song_line_index.txt)
+
+# Calculate the next song line index
+# 5 > 4 > 3 > 2 > 1 > 0
+NEXT_SONG_LINE_INDEX=$((CURRENT_SONG_LINE_INDEX - 1))
+if [[ $NEXT_SONG_LINE_INDEX < 0 ]]; then
+    NEXT_SONG_LINE_INDEX=$((LYRICS_COUNT - 1))
+fi
+
+# Save the next song line index to a file
+echo $NEXT_SONG_LINE_INDEX > next_song_line_index.txt
